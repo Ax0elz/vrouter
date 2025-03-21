4d409eea2757cc1ec7fbd5b4a41bcc2a7a2be98f
Ax0elz <alex.kotliarsky@gmail.com>
2025-03-20 22:39:26 -0600

    see if this solved anything

--- lib/src/core/vrouter_delegate.dart
+++ lib/src/core/vrouter_delegate.dart
@@ -4,23 +4,23 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:vrouter/src/core/errors.dart';
 import 'package:vrouter/src/core/navigator_extension.dart';
+import 'package:vrouter/src/core/root_vrouter.dart';
+import 'package:vrouter/src/core/route.dart';
 import 'package:vrouter/src/core/vlogs.dart';
 import 'package:vrouter/src/core/vpop_data.dart';
 import 'package:vrouter/src/core/vredirector.dart';
-import 'package:vrouter/src/core/root_vrouter.dart';
-import 'package:vrouter/src/core/route.dart';
 import 'package:vrouter/src/core/vroute_element.dart';
 import 'package:vrouter/src/core/vroute_element_node.dart';
+import 'package:vrouter/src/helpers/empty_page.dart';
 import 'package:vrouter/src/logs/vlog_printer.dart';
 import 'package:vrouter/src/logs/vlogs.dart';
 import 'package:vrouter/src/path_to_regexp/path_to_regexp.dart';
 import 'package:vrouter/src/vrouter_scope.dart';
-import 'package:vrouter/src/helpers/empty_page.dart';
 import 'package:vrouter/src/vrouter_vroute_elements.dart';
 import 'package:vrouter/src/vrouter_widgets.dart';
+import 'package:vrouter/src/wrappers/browser_helpers/browser_helpers.dart';
 import 'package:vrouter/src/wrappers/move_to_background.dart';
 import 'package:vrouter/src/wrappers/platform/platform.dart';
-import 'package:vrouter/src/wrappers/browser_helpers/browser_helpers.dart';
 
 import 'vrouter_sailor/vrouter_sailor.dart';
 
@@ -84,8 +84,7 @@ class VRouterDelegate extends RouterDelegate<RouteInformation>
     Future<void> Function(
       VRedirector vRedirector,
       void Function(Map<String, String> historyState) saveHistoryState,
-    )
-        beforeLeave = VoidVGuard.voidBeforeLeave,
+    ) beforeLeave = VoidVGuard.voidBeforeLeave,
     void Function(BuildContext context, String? from, String to) afterEnter =
         VoidVGuard.voidAfterEnter,
     Future<void> Function(VRedirector vRedirector) onPop =
@@ -1652,8 +1651,8 @@ class VRouterDelegate extends RouterDelegate<RouteInformation>
   /// Navigation state to app state
   @override
   Future<void> setNewRoutePath(RouteInformation routeInformation) async {
-    if (routeInformation.location != null && !_ignoreNextBrowserCalls) {
-      final newUrl = routeInformation.location!;
+    if (!_ignoreNextBrowserCalls) {
+      final newUrl = routeInformation.uri;
 
       final routeState = routeInformation.state as Map<String, dynamic>?;
 
@@ -1688,7 +1687,7 @@ class VRouterDelegate extends RouterDelegate<RouteInformation>
           ? VNavigationMethod.browserPush
           : VNavigationMethod.browserHistory;
 
-      final newUri = Uri.parse(newUrl);
+      final newUri = newUrl;
 
       // Update the app with the new url
       await _updateUrl(
@@ -1699,7 +1698,7 @@ class VRouterDelegate extends RouterDelegate<RouteInformation>
           VLogPrinter.show(
             VStoppedNavigationTo(
               vNavigationMethod: vNavigationMethod,
-              url: newUrl,
+              url: newUrl.toString(),
             ),
           );
 
@@ -1729,14 +1728,14 @@ class VRouterDelegate extends RouterDelegate<RouteInformation>
           VLogPrinter.show(
             VSuccessfulNavigationTo(
               vNavigationMethod: vNavigationMethod,
-              url: newUrl,
+              url: newUrl.toString(),
             ),
           );
 
           // If the navigation is successful, sync the vHistory
           if (isPush) {
             _vRouterScope.vHistory.pushLocation(
-              VRouteInformation(url: newUrl, state: newState),
+              VRouteInformation(url: newUrl.toString(), state: newState),
             );
           } else {
             _vRouterScope.vHistory.go(
@@ -1761,7 +1760,7 @@ class VRouterDelegate extends RouterDelegate<RouteInformation>
     }
 
     return RouteInformation(
-      location: url!,
+      uri: Uri.tryParse(url!),
       state: historyState,
     );
   }
